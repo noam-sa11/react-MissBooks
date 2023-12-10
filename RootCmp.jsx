3381dbf57e9b1933980659aa99d8b96cdc940bba
Noam Saar <noamsaar11@gmail.com>
2023-12-10 15:28:03 +0200

    Add RootComp.jsx

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -1,14 +1,30 @@
-import { Home } from './cmps/Home.jsx'
+import { AboutUs } from "./pages/AboutUs.jsx"
+import { BookIndex } from "./pages/BookIndex.jsx"
+import { Home } from "./pages/Home.jsx"
+
+const { useState } = React
 
 export function App() {
+    const [page, setPage] = useState('books')
+
     return (
-        <section className="app">
-            <header className="app-header">
-                <h1>My App</h1>
+        <section className="app main-layout">
+            <header className="app-header full main-layout">
+                <section>
+                    <h1>React MissBooks App</h1>
+                    <nav className="app-nav">
+                        <a onClick={() => setPage('home')} href="#">Home</a>
+                        <a onClick={() => setPage('about')} href="#">About</a>
+                        <a onClick={() => setPage('books')} href="#">Books</a>
+                    </nav>
+                </section>
             </header>
-            <main class="container">
-                <Home />
+
+            <main>
+                {page === 'home' && <Home />}
+                {page === 'about' && <AboutUs />}
+                {page === 'books' && <BookIndex />}
             </main>
         </section>
     )
-}
+} 
