3f0cd5281f0347e6d251143e50eefc7d888b7f1b
Noam Saar <noamsaar11@gmail.com>
2023-12-11 14:52:08 +0200

    Refactor BookList.jsx

--- cmps/BookList.jsx
+++ cmps/BookList.jsx
@@ -1,6 +1,22 @@
 import { BookPreview } from "./BookPreview.jsx"
+const { Link } = ReactRouterDOM
 
-export function BookList({ books, onRemoveBook, onSelectBookId, onEditSelectedBook }) {
+// export function BookList({ books, onRemoveBook, onSelectBookId, onEditSelectedBook }) {
+//     return (
+//         <ul className="book-list">
+//             {books.map((book, idx) =>
+//                 <li key={book.id} className="book-card">
+//                     <BookPreview book={book} idx={idx + 1}/>
+//                     <section>
+//                         <button onClick={() => onRemoveBook(book.id)}>Remove Book</button>
+//                         <button onClick={() => onSelectBookId(book.id)}>Details</button>
+//                     </section>
+//                 </li>
+//             )}
+//         </ul>
+//     )
+// }
+export function BookList({ books, onRemoveBook, onEditSelectedBook }) {
     return (
         <ul className="book-list">
             {books.map((book, idx) =>
@@ -8,7 +24,8 @@ export function BookList({ books, onRemoveBook, onSelectBookId, onEditSelectedBo
                     <BookPreview book={book} idx={idx + 1}/>
                     <section>
                         <button onClick={() => onRemoveBook(book.id)}>Remove Book</button>
-                        <button onClick={() => onSelectBookId(book.id)}>Details</button>
+                        <button><Link to={`/book/${book.id}`}>Details</Link></button>
+                        <button><Link to={`/book/edit/${book.id}`}>Edit</Link></button>
                     </section>
                 </li>
             )}
