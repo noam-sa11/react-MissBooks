46822fca0aa5906ff1ed6162c41f799251bc8f38
Noam Saar <noamsaar11@gmail.com>
2023-12-11 14:49:29 +0200

    Add Routing to RootCmp.jsx

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -1,28 +1,34 @@
+// const Router = ReactRouterDOM.BrowserRouter
+const Router = ReactRouterDOM.HashRouter 
+const { Routes, Route } = ReactRouterDOM
+
 import { AppHeader } from "./cmps/AppHeader.jsx"
+import { UserMsg } from "./cmps/UserMsg.jsx"
 import { AboutUs } from "./pages/AboutUs.jsx"
+import { BookDetails } from "./pages/BookDetails.jsx"
 import { BookIndex } from "./pages/BookIndex.jsx"
+import { BookEdit } from "./pages/BookEdit.jsx"
 import { Home } from "./pages/Home.jsx"
 
-const { useState } = React
 
 export function App() {
-    const [page, setPage] = useState('books')
-
-    function onSetPage(page) {
-        setPage(page)
-    }
-
     return (
-        <section className="app main-layout">
-            <header className="app-header full main-layout">
-                <AppHeader onSetPage={onSetPage}/>
-            </header>
+        <Router>
+            <section className="app main-layout">
+                <AppHeader />
+                <main>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/about" element={<AboutUs />} />
+                        <Route path="/book" element={<BookIndex />} />
+                        <Route path="/book/:bookId" element={<BookDetails />} />
+                        <Route path="/book/edit" element={<BookEdit />} />
+                        <Route path="/book/edit:bookId" element={<BookEdit />} />
+                    </Routes>
+                </main>
+             <UserMsg />
+            </section>
+        </Router>
 
-            <main>
-                {page === 'home' && <Home />}
-                {page === 'about' && <AboutUs />}
-                {page === 'books' && <BookIndex />}
-            </main>
-        </section>
     )
 } 
