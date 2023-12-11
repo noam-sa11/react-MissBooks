358241c7f71b1ddd9b2c6318977fab60f8f6b729
Noam Saar <noamsaar11@gmail.com>
2023-12-11 17:40:00 +0200

    Add Filter Styling

--- cmps/BookFilter.jsx
+++ cmps/BookFilter.jsx
@@ -40,31 +40,63 @@ export function BookFilter({ filterBy, onSetFilter }) {
     const { title, price } = filterByToEdit
 
     return (
-        <section className="book-filter">
-            <h2>Filter Our Books</h2>
-            <form onSubmit={onSetFilterBy} >
-                <div className="title-filter">
-                    <label htmlFor="title">Title: </label>
-                    <input 
-                        value={title} 
-                        onChange={handleChange} 
-                        type="text" 
-                        id="title" 
-                        name="title" />
-                </div>
+        // <section className="book-filter">
+        //     <h2>Filter Our Books</h2>
+        //     <form onSubmit={onSetFilterBy} >
+        //         <div className="title-filter">
+        //             <label htmlFor="title">Title: </label>
+        //             <input 
+        //                 value={title} 
+        //                 onChange={handleChange} 
+        //                 type="text" 
+        //                 id="title" 
+        //                 name="title" />
+        //         </div>
 
-                <div className="price-filter">
-                    <label htmlFor="price">Max Price: <span>{`(${rangeDisplay})`}</span></label>
-                    <input 
-                        value={price || rangeDisplay}
-                        onChange={handleChange}
-                        type="range"
-                        id="price"
-                        name="price"
-                        min="0"
-                        max="250" />
-                </div>
-            </form>
-        </section>
+        //         <div className="price-filter">
+        //             <label htmlFor="price">Max Price: <span>{`(${rangeDisplay})`}</span></label>
+        //             <input 
+        //                 value={price || rangeDisplay}
+        //                 onChange={handleChange}
+        //                 type="range"
+        //                 id="price"
+        //                 name="price"
+        //                 min="0"
+        //                 max="250" />
+        //         </div>
+        //     </form>
+        // </section>
+            <fieldset className="book-filter">
+                <legend>Filter Our Books</legend>
+                <form onSubmit={onSetFilterBy} >
+                    <fieldset>
+                        <legend>Title Filter</legend>
+                        <div className="title-filter">
+                            <label htmlFor="title"></label>
+                            <input
+                                value={title}
+                                onChange={handleChange}
+                                type="text"
+                                id="title"
+                                name="title" />
+                        </div>
+                    </fieldset>
+
+                    <fieldset>
+                        <legend>Max Price Filter</legend>
+                        <div className="price-filter">
+                            <label htmlFor="price"><span>{`(${rangeDisplay})`}</span></label>
+                            <input
+                                value={price || rangeDisplay}
+                                onChange={handleChange}
+                                type="range"
+                                id="price"
+                                name="price"
+                                min="0"
+                                max="250" />
+                        </div>
+                    </fieldset>
+                </form>
+            </fieldset>
     )
 }
