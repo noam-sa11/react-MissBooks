83cfa2a8f34c45c1207e5f9140575efa12c40fa3
Noam Saar <noamsaar11@gmail.com>
2023-12-11 14:52:34 +0200

    Refactor AppHeader.jsx

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -1,12 +1,32 @@
-export function AppHeader({ onSetPage }) {
+const { NavLink } = ReactRouterDOM
+
+// export function AppHeader({ onSetPage }) {
+//     return (
+//         <header className="app-header full main-layout">
+//             <section>
+//                 <h1>MissBooks</h1>
+//                 <nav className="app-nav">
+//                     <a onClick={() => onSetPage('home')} href="#">Home</a>
+//                     <a onClick={() => onSetPage('about')} href="#">About</a>
+//                     <a onClick={() => onSetPage('books')} href="#">Books</a>
+//                 </nav>
+//             </section>
+
+//         </header>
+//     )
+// }
+
+export function AppHeader() {
     return (
-        <section>
-            <h1>MissBooks</h1>
-            <nav className="app-nav">
-                <a onClick={() => onSetPage('home')} href="#">Home</a>
-                <a onClick={() => onSetPage('about')} href="#">About</a>
-                <a onClick={() => onSetPage('books')} href="#">Books</a>
-            </nav>
-        </section>
+        <header className="app-header full main-layout">
+            <section>
+                <h1>MissBooks</h1>
+                <nav className="app-nav">
+                    <NavLink to="/" >Home</NavLink>
+                    <NavLink to="/about" >About</NavLink>
+                    <NavLink to="/book" >Books</NavLink>
+                </nav>
+            </section>
+        </header>
     )
 }
